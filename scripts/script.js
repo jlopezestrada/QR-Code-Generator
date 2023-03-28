9f8dbc2f42cf534e453a38c8ba6c0f8d75846e51
José Ángel <jose.a.l.e@hotmail.com>
2023-03-28 10:59:13 +0200

    Add Download button

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,5 @@ function generateQRCode() {
     var qrText = document.getElementById("qr-text").value;
     var qrCodeURL = "https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=" + encodeURIComponent(qrText);
     document.getElementById("qr-code").innerHTML = '<img src="' + qrCodeURL + '">';
+    document.getElementById("qr-download").innerHTML = '<a href="' + qrCodeURL + '" download="qr-code"><button type="button">Download</button></a>'
   }
