7793187300e8db253eb5302c6f73db6bcfa17ec5
José Ángel <jose.a.l.e@hotmail.com>
2023-04-20 11:33:33 +0200

    Add basic data validation and limit

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,12 @@
 function generateQRCode() {
+  try {
     var qrText = document.getElementById("qr-text").value;
-    var qrCodeURL = "https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=" + encodeURIComponent(qrText);
-    document.getElementById("qr-code").innerHTML = '<img src="' + qrCodeURL + '">';
-    document.getElementById("qr-download").innerHTML = '<a href="' + qrCodeURL + '" download="qr-code"><button type="button">Download</button></a>'
-  }
+    if (qrText != "") {
+      var qrCodeURL = "https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=" + encodeURIComponent(qrText);
+      document.getElementById("qr-code").innerHTML = '<img src="' + qrCodeURL + '">';
+      document.getElementById("qr-download").innerHTML = '<a href="' + qrCodeURL + '" download="qr-code"><button type="button">Download</button></a>'
+    }
+  } catch (err) {
+    console.log(err)   
+  } 
+}
